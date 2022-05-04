4723b789ef3389cedae2e143e8f3d21a87f65213
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-05-04 11:16:33 +0200

    first commit

---
+++ backend/src/test/java/zw/co/enhanceit/MultiUserAuth/MultiUserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.enhanceit.MultiUserAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiUserAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
