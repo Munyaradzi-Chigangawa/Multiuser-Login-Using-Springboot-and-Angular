2c21b81a06ea13acf22447735c6d0fabacb26701
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-05-12 15:34:03 +0200

    added service for user

--- frontend/src/app/service/auth.service.ts
+++ frontend/src/app/service/auth.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { User } from '../model/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  loginUrl: string = '';
+  registerUrl: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.loginUrl = 'http://localhost:8080/auth/login';
+    this.registerUrl = 'http://localhost:8080/auth/register';
+  }
+
+  login(user: User): Observable<any> {
+    return this.http.post<any>(this.loginUrl, user);
+  }
+
+  register(user: User): Observable<any> {
+    return this.http.post<any>(this.registerUrl, user);
+  }
 }
